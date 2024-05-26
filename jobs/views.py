bb7be797a6623c1f5f3fff6727ee5db050497dee
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-05-26 18:31:51 +0530

    Creating jobslist and jobsdetail classes

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, status, viewsets
+from .models import Job
+from .serializers import JobSerializer
 
 # Create your views here.
+
+class JobsList(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobsViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    
